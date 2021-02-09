6e9a9688d19a7fe1f6dfcb45dca6564cf16c63d1
AndreiYazadzhi <belator251000@gmail.com>
2021-02-09 11:38:04 +0200

    Exception fix

--- src/main/java/spring/dao/UserDaoImpl.java
+++ src/main/java/spring/dao/UserDaoImpl.java
@@ -31,7 +31,7 @@ public class UserDaoImpl implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't insert user " + user, e);
+            throw new DataProcessException("Can't insert user " + user, e);
         } finally {
             if (session != null) {
                 session.close();
